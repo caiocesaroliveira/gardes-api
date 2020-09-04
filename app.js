99eeaa37e37d9ce65c40c13e6a7113a7447e577f
caioc <caioc@DESKTOP-34UIDD6>
2020-09-03 21:06:06 -0300

    correção do cors

--- app.js
+++ app.js
@@ -24,14 +24,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    a,
+    origin: "https://front-grades-fullstack.herokuapp.com",
   })
 )
 
 app.use(gradeRouter)
-app.get("/", (req, res) => {
-  res.send("API em execucao")
-})
 
 app.listen(process.env.PORT || 3333, () => {})
